6c083932e9b416b7759305f6bcc1c1f0761a78e0
gexayr <mgexayr@smartbet.am>
2023-04-10 14:36:35 +0400

    test

--- src/db.js
+++ src/db.js
@@ -11,13 +11,6 @@ module.exports = {
     mongoose.set('useCreateIndex', true);
     // Use the new server discovery & monitoring engine
     mongoose.set('useUnifiedTopology', true);
-
-    mongoose.connect('mongodb+srv://gexo777:I52I6KX79siMdGh6@cluster0.mwpbjzh.mongodb.net/?retryWrites=true&w=majority',
-        { useNewUrlParser: true })
-        .then(() => console.log("Mongodb connected"))
-        .catch(err => console.log(err));
-
-
     // Connect to the DB
     mongoose.connect(DB_HOST);
     // Log an error if we fail to connect
